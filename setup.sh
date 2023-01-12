a3f5b8bcc9b6470af31e8d43dc41c82ea977dd2c
Jay Kumar Patil <itsme@Jays-MacBook-Pro.local>
2023-01-12 17:17:17 +0530

    Script Pasued

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ set -x
 
 BASE_DIR_PATH="${HOME}/openshift-cluster"
 
-exec ${BASE_DIR_PATH}/scripts/00_Prerequisite.sh
-exec ${BASE_DIR_PATH}/scripts/01_BIND_DNS.sh
-exec ${BASE_DIR_PATH}/scripts/02_DHCP.sh
-exec ${BASE_DIR_PATH}/scripts/03_Apache_Web_Server.sh
-exec ${BASE_DIR_PATH}/scripts/04_HAProxy.sh
-exec ${BASE_DIR_PATH}/scripts/05_NFS.sh
-exec ${BASE_DIR_PATH}/scripts/06_Generate_and_host_Install_files.sh
+bash ${BASE_DIR_PATH}/scripts/00_Prerequisite.sh
+bash ${BASE_DIR_PATH}/scripts/01_BIND_DNS.sh
+bash ${BASE_DIR_PATH}/scripts/02_DHCP.sh
+bash ${BASE_DIR_PATH}/scripts/03_Apache_Web_Server.sh
+bash ${BASE_DIR_PATH}/scripts/04_HAProxy.sh
+bash ${BASE_DIR_PATH}/scripts/05_NFS.sh
+bash ${BASE_DIR_PATH}/scripts/06_Generate_and_host_Install_files.sh
